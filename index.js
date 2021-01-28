6b9aff9d64b28e9617077a949f8617d0e2a7f19f
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-01-28 19:15:41 +0000

    cartas aleatorias

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import "/workspace/generator-cards/style.css"
 
-window.onload = function(){
-    this.console.log("todo esta sirviendo");
 
+window.onload = () =>{
+       document.querySelector('.card').classList.add(generateRandomSuit());
+       document.querySelector('.card').innerHTML = (generateRandomNumber());
+  
 };
- 
+
+
+ let generateRandomNumber = () => {
+     let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+     let indexNumbers = Math-floor(Math.random() * numbers.length);
+    return numbers [indexNumbers];
+ };
+
+  let generateRandomSuit =() =>{
+     let suit = ["diamond","spade","heart","club"];    
+     let indexSuit = Math-floor(Math.random() * suit.length);
+     return suit [indexSuit];
+ };
